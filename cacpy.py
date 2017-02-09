5217db272beb6c8df479b7fea9346fa553e02e86
Hiromasa Ihara <iharahiromasa@gmail.com>
2017-02-10 01:56:24 +0900

    quick commit

--- cacpy.py
+++ cacpy.py
@@ -27,7 +27,7 @@ class API() :
       raise Exception("{:d} {:s}".format(ret.status_code, ret_obj["error_description"]))
     return ret_obj['data']
 
-  def listtemplates() :
+  def listtemplates(self) :
     payload = { 'key':self.key, 'login':self.login }
     ret = requests.get("{:s}/{:s}/{:s}".format(self.api_url, self.version, "listtemplates.php"), params=payload)
     ret_obj = json.loads(ret.text)
@@ -38,6 +38,7 @@ class API() :
   def listtasks(self) :
     payload = { 'key':self.key, 'login':self.login }
     ret = requests.get("{:s}/{:s}/{:s}".format(self.api_url, self.version, "listtasks.php"), params=payload)
+    print ret.text
     ret_obj = json.loads(ret.text)
     if ret.status_code != 200 :
       raise Exception("{:d} {:s}".format(ret.status_code, ret_obj["error_description"]))
@@ -58,9 +59,25 @@ class API() :
     pass
   def runmode(self) :
     pass
-  def cloudpro_build(self) :
-    pass
-  def cloudpro_delete(self) :
-    pass
+
+  def cloudpro_build(self, cpu, ram, storage, os) :
+    payload = { 'key':self.key, 'login':self.login, 'cpu':cpu, 'ram':ram, 'storage':storage, 'os':os }
+    ret = requests.post("{:s}/{:s}/{:s}".format(self.api_url, self.version, "cloudpro/build.php"), data=payload)
+    ret_obj = json.loads(ret.text)
+    if ret.status_code != 200 :
+      raise Exception("{:d} {:s}".format(ret.status_code, ret_obj["error_description"]))
+
+  def cloudpro_delete(self, sid) :
+    payload = { 'key':self.key, 'login':self.login, 'sid':sid }
+    ret = requests.post("{:s}/{:s}/{:s}".format(self.api_url, self.version, "cloudpro/delete.php"), data=payload)
+    ret_obj = json.loads(ret.text)
+    if ret.status_code != 200 :
+      raise Exception("{:d} {:s}".format(ret.status_code, ret_obj["error_description"]))
+
   def cloudpro_resources(self) :
-    pass
+    payload = { 'key':self.key, 'login':self.login }
+    ret = requests.get("{:s}/{:s}/{:s}".format(self.api_url, self.version, "cloudpro/resources.php"), params=payload)
+    ret_obj = json.loads(ret.text)
+    if ret.status_code != 200 :
+      raise Exception("{:d} {:s}".format(ret.status_code, ret_obj["error_description"]))
+    return ret_obj['data']
